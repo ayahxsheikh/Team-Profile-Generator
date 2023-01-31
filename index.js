67911f635b338d34dad65b6950b8f537a0657b8f
cc-aeya <aaminah.x@icloud.com>
2023-01-31 18:11:54 +0000

    created js file for generateEngineer

--- index.js
+++ index.js
@@ -33,37 +33,6 @@ function mainMenu(){
 });
 };
 
-
-
-// function to generate new engineer based on user input
-function generateEngineer(){
-    return inquirer.prompt([
-        {
-            name: "name",
-            message: "Enter the new Engineer's name"
-        },
-        {
-            name: "id",
-            message: "Enter the new Engineer's employee Id"
-        },
-        {
-            name: "email",
-            message: "Enter the new Engineer's email address"
-        },
-        {
-            name: "github",
-            message: "Lastly, enter the Engineer's GitHub username"
-        }
-    ]).then(data => {
-        let engineer = new Engineer(data.name, data.id, data.email, data.github);
-        team.push(engineer);
-        console.log(engineer);
-
-    // directs user back to adding another member
-        buildTeam();
-    });
-};
-
 // function creates new intern member based on user input
 function generateIntern(){
     return inquirer.prompt([
---
+++ starter/team/generateEngineer.js
@@ -0,0 +1,33 @@
+const Engineer = require("../lib/Engineer");
+const buildTeam = require("./buildTeam");
+
+// function to generate new engineer based on user input
+function generateEngineer(){
+    return inquirer.prompt([
+        {
+            name: "name",
+            message: "Enter the new Engineer's name"
+        },
+        {
+            name: "id",
+            message: "Enter the new Engineer's employee Id"
+        },
+        {
+            name: "email",
+            message: "Enter the new Engineer's email address"
+        },
+        {
+            name: "github",
+            message: "Lastly, enter the Engineer's GitHub username"
+        }
+    ]).then(data => {
+        let engineer = new Engineer(data.name, data.id, data.email, data.github);
+        team.push(engineer);
+        console.log(engineer);
+
+    // directs user back to adding another member
+        buildTeam();
+    });
+};
+
+module.exports = generateEngineer;
